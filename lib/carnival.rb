bc2362d2ef8065b9717dfb1ce87ab68036108357
Antonio Anderson Souza <antonioams@gmail.com>
2014-03-23 00:59:01 -0300

    Vários ajustes para deixar tudo com o nome carnival

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,4 +1,10 @@
-Bundler.require
+require "rubygems"
+require "devise"
+require "simple_form"
+require "inherited_resources"
+require "will_paginate"
+require "omniauth-facebook"
+require "omniauth-google-oauth2"
 require "carnival/engine"
 require "carnival/config"
 require "carnival/routes"
