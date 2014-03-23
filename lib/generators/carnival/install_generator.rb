a5718085f5024648fe922c340441c4d51475084d
Antonio Anderson Souza <antonioams@gmail.com>
2014-03-23 01:27:23 -0300

    Vários ajustes para deixar tudo com o nome carnival

--- lib/generators/carnival/install_generator.rb
+++ lib/generators/carnival/install_generator.rb
@@ -2,17 +2,17 @@ module Carnival
   module Generators
     class InstallGenerator < Rails::Generators::Base
       desc "Install the Carnival Engine"
-      source_root File.expand_path("../../templates", __FILE__)
+      source_root File.expand_path("../templates", __FILE__)
 
       def install
         route "mount_my_engine_at 'admin'"
         rake 'carnival_engine:install:migrations'
         puts Rails.root
-        copy_file "../../../../config/locales/carnival.en.yml", "config/locales/carnival.en.yml"
-        copy_file "../../../../config/locales/carnival.pt.yml", "config/locales/carnival.pt.yml"
-        copy_file "../../../../config/locales/devise.pt.yml", "config/locales/devise.pt.yml"
-        copy_file "../../../../config/locales/devise.en.yml", "config/locales/devise.en.yml"
-        copy_file "carnival_initializer.rb", "config/initializers/carnival_initializer.rb"
+        template "../../../config/locales/carnival.en.yml", "config/locales/carnival.en.yml"
+        template "../../../config/locales/carnival.pt.yml", "config/locales/carnival.pt.yml"
+        template "../../../config/locales/devise.pt.yml", "config/locales/devise.pt.yml"
+        template "../../../config/locales/devise.en.yml", "config/locales/devise.en.yml"
+        template "carnival_initializer.rb", "config/initializers/carnival_initializer.rb"
       end
     end
   end
