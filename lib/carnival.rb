3e340382ff78255f9b51c5450036cda0f8239c0e
Antonio Anderson Souza <antonioams@gmail.com>
2014-03-23 00:45:19 -0300

    Vários ajustes para deixar tudo com o nome carnival

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,8 +1,9 @@
+Bundler.require
+require "devise"
+require "simple_form"
 require "carnival/engine"
 require "carnival/config"
 require "carnival/routes"
 require "carnival/version"
-require "devise"
-require "simple_form"
 module Carnival
 end
