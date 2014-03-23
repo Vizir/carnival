7f4858f23773fad1c0f4e4e01be49e19e69fd214
Antonio Anderson Souza <antonioams@gmail.com>
2014-03-23 01:29:43 -0300

    Vários ajustes para deixar tudo com o nome carnival

--- lib/generators/carnival/install_generator.rb
+++ lib/generators/carnival/install_generator.rb
@@ -8,10 +8,10 @@ module Carnival
         route "mount_my_engine_at 'admin'"
         rake 'carnival_engine:install:migrations'
         puts Rails.root
-        template "../../../config/locales/carnival.en.yml", "config/locales/carnival.en.yml"
-        template "../../../config/locales/carnival.pt.yml", "config/locales/carnival.pt.yml"
-        template "../../../config/locales/devise.pt.yml", "config/locales/devise.pt.yml"
-        template "../../../config/locales/devise.en.yml", "config/locales/devise.en.yml"
+        template "../../../../config/locales/carnival.en.yml", "config/locales/carnival.en.yml"
+        template "../../../../config/locales/carnival.pt.yml", "config/locales/carnival.pt.yml"
+        template "../../../../config/locales/devise.pt.yml", "config/locales/devise.pt.yml"
+        template "../../../../config/locales/devise.en.yml", "config/locales/devise.en.yml"
         template "carnival_initializer.rb", "config/initializers/carnival_initializer.rb"
       end
     end
