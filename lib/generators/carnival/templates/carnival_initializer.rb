49a95672abc43a8d9b86cf745b3e043e378eb5fb
diegonakamashi <diegonakamashi@gmail.com>
2014-06-04 11:48:41 -0300

    Comment the custom assets files

--- lib/generators/carnival/templates/carnival_initializer.rb
+++ lib/generators/carnival/templates/carnival_initializer.rb
@@ -47,6 +47,12 @@ Carnival::Config.omniauth_providers = [:facebook, :google_oauth2]
 Carnival::Config.omniauth = {facebook: ["324810390938005", "3c16625e74189a3708cc586dc050a6b2"],
                             google_oauth2: ['431077382019-mumumjahr5cn6cooubtskc6ohael7923.apps.googleusercontent.com', 'ilH4B-KXN3tqG6qF9gGN1F_J']}
 
-Carnival::Config.custom_css_files = ["samplefile.css"]
+# Custom CSS Files
+# Carnival::Config.custom_css_files = ["samplefile.css"]
 
-Carnival::Config.custom_javascript_files = ["samplefile.js"]
+# Custom Javascript Files
+# Carnival::Config.custom_javascript_files = ["samplefile.js"]
+
+# Determine the super class of AdminUser
+#Carnival::Config.ar_admin_user_class = ActiveRecord::Base
+
