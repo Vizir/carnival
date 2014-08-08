76d58b12ec283235c9c4f2a25a44af64da5d4817
cartolari <bruno.cartolari@gmail.com>
2014-08-08 18:36:43 -0300

    Correcting Carnival date format on edit forms

--- app/inputs/admin_date_input.rb
+++ app/inputs/admin_date_input.rb
@@ -3,8 +3,9 @@ class AdminDateInput < SimpleForm::Inputs::Base
   include AdminBaseInput
 
   def input
+    format = if column.type == :datetime then '%Y/%m/%d %H:%M' else '%Y/%m/%d' end
     default_input_html_options = {class: "#{column.type}picker",
-      value: @builder.object.send(attribute_name).nil? ? "" : @builder.object.send(attribute_name).strftime('%d/%m/%Y')}
+      value: @builder.object.send(attribute_name).nil? ? "" : @builder.object.send(attribute_name).strftime(format)}
     options = merge_options(default_input_html_options, input_html_options)
     return "#{@builder.text_field(attribute_name, options)}".html_safe
   end
