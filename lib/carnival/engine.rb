31b6fb3a0c11cc246465705d9269ff033ccdf51c
cartolari <bruno.cartolari@gmail.com>
2014-08-11 18:32:26 -0300

    Leaving migrations inside the Carnival engine

--- lib/carnival/engine.rb
+++ lib/carnival/engine.rb
@@ -1,5 +1,13 @@
 module Carnival
   class Engine < ::Rails::Engine
     #isolate_namespace Carnival
+    #
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
