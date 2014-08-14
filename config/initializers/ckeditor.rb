18b414f5c1efe0c92636c42715e1cb5f96cc8c14
cartolari <bruno.cartolari@gmail.com>
2014-08-14 19:04:20 -0300

    Ckeditor assets languages

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -34,6 +34,6 @@ Ckeditor.setup do |config|
 
   # To reduce the asset precompilation time, you can limit plugins and/or languages to those you need:
   # By default: nil (no limit)
-  # config.assets_languages = ['en', 'uk']
+  config.assets_languages = ['en', 'uk', 'pt', 'pt-br']
   # config.assets_plugins = ['image', 'smiley']
 end
