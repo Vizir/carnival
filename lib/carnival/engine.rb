a63d3ff0d8ea79410bf5ea565319841e724659ee
cartolari <bruno.cartolari@gmail.com>
2014-09-26 13:18:15 -0300

    Removing Carnival migration auto inclusion

--- lib/carnival/engine.rb
+++ lib/carnival/engine.rb
@@ -1,11 +1,5 @@
 module Carnival
   class Engine < ::Rails::Engine
     #isolate_namespace Carnival
-    #
-    initializer :append_migrations do |app|
-      config.paths["db/migrate"].expanded.each do |expanded_path|
-        app.config.paths["db/migrate"] << expanded_path
-      end
-    end
   end
 end
