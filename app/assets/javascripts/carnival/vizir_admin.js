1450a1c2b39d1699a57f7ebf1f607c49509fb6d5
cartolari <bruno.cartolari@gmail.com>
2014-09-26 20:23:46 -0300

    Setting the default width for selects to 100%

--- app/assets/javascripts/carnival/vizir_admin.js
+++ app/assets/javascripts/carnival/vizir_admin.js
@@ -145,7 +145,7 @@ var pageLoad = function(){
   })
 
 
-  $(".carnival-select").select2();
+  $(".carnival-select").select2({width: '100%'});
   $(".popup").click(function(e){
     e.preventDefault()
     var url = $(this).attr('href')
@@ -200,7 +200,7 @@ function setupDateFields(){
 
 var formLoad = function(){
   setupDateFields();
-  $('select').select2();
+  $('select').select2({width: '100%'});
   $('input.previewable').change(function() {
     var that = $( this );
     var reader = new FileReader();
@@ -217,7 +217,7 @@ var formLoad = function(){
       img.attr('src', '');
     }
   });
-  $('.carnival-select').select2();
+  $('.carnival-select').select2({width: '100%'});
   selectRemote();
 }
 
