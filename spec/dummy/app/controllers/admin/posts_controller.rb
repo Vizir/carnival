b991a2c6cf80ab524fb77c375f3e97fac627e5ab
cartolari <bruno.cartolari@gmail.com>
2015-03-15 00:49:08 -0300

    First Carnival feature spec
    * Better late than never!

--- spec/dummy/app/controllers/admin/posts_controller.rb
+++ spec/dummy/app/controllers/admin/posts_controller.rb
@@ -1,3 +1,7 @@
 class Admin::PostsController < Carnival::BaseAdminController
   layout 'carnival/admin'
+
+  def permitted_params
+    params.permit(post: [:title, :content])
+  end
 end
--- spec/features/create_spec.rb
+++ spec/features/create_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 feature 'User creates a new model' do
   scenario 'successfully' do
-    visit root_path
+    visit admin_posts_path
+
+    within('.empty-result') { click_on 'New' }
+    fill_in 'post_title', with: 'Carnival'
+    fill_in 'post_content', with: 'A new admin engine'
+    # status
+
+    expect { click_on 'Create' }.to change { Post.count }.by(1)
+  end
+
+  scenario 'incompletely' do
+    visit admin_posts_path
+
+    within('.empty-result') { click_on 'New' }
+    fill_in 'post_title', with: 'Carnival'
+
+    expect { click_on 'Create' }.to_not change { Post.count }
+    expect(current_path).to eq(admin_posts_path)
+    expect(page).to have_no_text('Title can\'t be blank')
+    expect(page).to have_text('Content can\'t be blank')
   end
 end
