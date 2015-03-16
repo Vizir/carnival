ab76e154bcfe50829dd5e1d4e4dc006566b1a7be
cartolari <bruno.cartolari@gmail.com>
2015-03-15 23:14:23 -0300

    Remove unnecessary namespace ref in BaseAdminController

--- app/controllers/carnival/base_admin_controller.rb
+++ app/controllers/carnival/base_admin_controller.rb
@@ -14,7 +14,7 @@ module Carnival
       respond_to do |format|
         format.html do
           @records = @query_service.get_query
-          @paginator = Carnival::Paginator.new @query_form.page, @query_service.page_count
+          @paginator = Paginator.new @query_form.page, @query_service.page_count
         end
         format.csv do
           @records = @query_service.records_without_pagination
