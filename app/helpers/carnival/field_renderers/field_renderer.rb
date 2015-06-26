8441cc864fc8e6f049f2508f0b91fef0d7a904c2
cartolari <bruno.cartolari@gmail.com>
2015-06-26 15:27:14 -0300

    Pull up related_presenter method in FieldRenderer

--- app/helpers/carnival/field_renderers/field_renderer.rb
+++ app/helpers/carnival/field_renderers/field_renderer.rb
@@ -6,7 +6,19 @@ module Carnival::FieldRenderers
     end
 
     def render_field(model)
-     { field_type: @presenter.field_type(@field_name), value: model.send(@field_name) }
+      { field_type: @presenter.field_type(@field_name), value: model.send(@field_name) }
+    end
+
+    protected
+
+    def related_presenter
+      related_presenter_name = @presenter.get_related_class(field.association_name)
+      related_presenter_name.gsub!(/.*[(::)\/]/, '')
+      @presenter.presenter_to_field_sym(related_presenter_name)
+    end
+
+    def field
+      @presenter.get_field(@field_name)
     end
   end
 end
